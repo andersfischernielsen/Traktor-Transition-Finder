71e214a4808fbf26dcfe3b8bf8f0da24b698b90a
Anders Fischer-Nielsen <andersfischern@me.com>
2015-10-06 15:08:43 +0200

    Implemented key mixing for four keys.

--- Project/Traktor/Program.fs
+++ Project/Traktor/Program.fs
@@ -44,16 +44,34 @@ let rec createGraph (original: Song list) (list:Song list) acc =
                    createGraph original ss ((song, edgesFromSong) :: acc)
     | []      -> acc
 
+let weightForKey (key, other) = 
+    match (key, other) with //TODO: Define key rules and add more key rules.
+            | ("1d", other)      -> match other with
+                                    | "1m" | "2d" | "12d" -> 0 //Major to minor, one key up/down.
+                                    | "3d" | "8d"         -> 0 //One semitone, two semitones up.
+                                    | "10d"               -> 0 //Since major, three keys down.
+                                    | _                   -> 20 //Keys do not match up, add 20 to weight.
+            | ("1m", other)      -> match other with
+                                    | "1d" | "2m" | "12m" -> 0 //Major to minor, one key up/down.
+                                    | "3m" | "8m"         -> 0 //One semitone, two semitones up.
+                                    | "4m"                -> 0 //Since minor, three keys UP.
+                                    | _                   -> 20 //Keys do not match up, add 20 to weight.
+            | ("2d", other)      -> match other with
+                                    | "2m" | "3d" | "1d"  -> 0
+                                    | "4d" | "9d"         -> 0 
+                                    | "11d"               -> 0
+                                    | _                   -> 20 
+            | ("2m", other)      -> match other with
+                                    | "2d" | "3m" | "1m"  -> 0 
+                                    | "4m" | "9m"         -> 0 
+                                    | "5m"                -> 0
+                                    | _                   -> 20 
+            | _                 -> 10000
+
 let rec calculateWeights list acc = 
     let calculateWeight edge = 
         let bpmWeight = System.Math.Abs (edge.From.BPM - edge.To.BPM)
-        let keyWeight = 
-            match (edge.From.Key, edge.To.Key) with
-            | ("1d", "1m")      -> 0            //TODO: Define key rules.
-            | ("1d", "2d")      -> 0
-            | ("1d", "12d")     -> 0
-            | ("1d", "2m")      -> 10
-            | _                 -> 10000
+        let keyWeight = weightForKey (edge.From.Key, edge.To.Key)
 
         (int) bpmWeight + keyWeight
 
