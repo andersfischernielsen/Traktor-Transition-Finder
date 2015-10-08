2c442cc9d1fe63de4cadb5668f6c636dbed9c7fb
Anders Fischer-Nielsen <andersfischern@me.com>
2015-10-08 17:41:23 +0200

    Restructured functions for readability. Recalculate weights on path change.

--- Project/Traktor/Server.fs
+++ Project/Traktor/Server.fs
@@ -16,39 +16,75 @@ open Newtonsoft.Json.Serialization
 type SuaveTask<'a> = Async<'a option>
 type WebPart = HttpContext -> SuaveTask<HttpContext>
 
+///JSON data type
+type SongResponse = { Song : Song; Transitions : Song list }
+
 ///Convert a byte array into a string.
 let byteArrayAsString s = System.Text.Encoding.ASCII.GetString(s)
+
+///Take n elements from a given list until there are no more elements.
+let take n list = 
+    let rec takeAcc n list acc = 
+        match list with
+        | x::xs     when n > 0 -> takeAcc (n-1) xs (x::acc)
+        | _                    -> acc
+    takeAcc n list []
 
 ///Convert a given object into JSON.
 let asJson v = 
     let jsonSerializerSettings = new JsonSerializerSettings()
     jsonSerializerSettings.ContractResolver <- new CamelCasePropertyNamesContractResolver()
-
     JsonConvert.SerializeObject(v, jsonSerializerSettings) |> OK >>= Writers.setMimeType "application/json; charset=utf-8"
+
+///Deserialise a given object from JSON.
+let fromJson v = 
+    let jsonSerializerSettings = new JsonSerializerSettings()
+    jsonSerializerSettings.ContractResolver <- new CamelCasePropertyNamesContractResolver()
+    JsonConvert.DeserializeObject(v, jsonSerializerSettings)
 
 //Mutable graph of songs.
 let mutable graph = []
 
-//Search using the current graph.
-let search s = Search.search s graph |> List.map (fun x -> String.Format("{0} by {1}", (fst x).Title, (fst x).Artist))
 
 
 ///Set a new path to the collection from an incoming HTTP POST. Graph will be rebuilt.
 let setCollectionString s = 
     let asString = byteArrayAsString s
-    graph <- CollectionParser.parseCollection asString |> Graph.buildGraph 
+    let g = Graph.buildGraph <| CollectionParser.parseCollection asString 
+    let withWeights = Graph.calculateWeights g
+    graph <- withWeights
+   
+///Find a given (Song * Edge list) tuple with the given AudioId.
+let getById id graph = List.find (fun x -> (fst x).AudioId = id) graph
 
+///Get the n best transitions from a given (Song * Edge list) tuple.
+let bestTransitions n edges = 
+    edges |> List.sortBy (fun x -> x.Weight) 
+          |> take n 
+          |> List.map (fun x -> x.To)
 
-//Setup web server.
+///Search for a song using the current graph. Returns the result as a JSON HttpContext option.
+let searchAndReturnAsJsonString searchTerm = 
+    let search s = Search.search s graph |> List.map (fun x -> fst x)
+    search searchTerm |> asJson
+
+let getFiveBestTransitionsFromId id = 
+    let tuple = getById id graph
+    let transitions = bestTransitions 5 <| snd tuple
+    let response = { Song = fst tuple; Transitions = transitions }
+    response |> asJson
+
+///Setup web server.
 let app =
   choose
     [ GET >>= choose
         [ path "/" >>= OK "Web server is running."
-          pathScan "/search/%s" (fun (searchTerm) -> search searchTerm |> asJson)
+          pathScan "/search/%s" (fun searchTerm -> searchAndReturnAsJsonString searchTerm)
         ]
       POST >>= choose
         [ path "/collection" >>= request (fun req -> setCollectionString <| req.rawForm; 
-                                                     OK "Collection path changed.") 
+                                                     OK "Collection path succesfully set.") 
+          pathScan "/choose/%s" (fun id -> getFiveBestTransitionsFromId id)
         ]
     ]
 
