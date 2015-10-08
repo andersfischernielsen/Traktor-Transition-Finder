eeb38405c01c55cc8f2dece1d96615ec36c4b7e8
Anders Fischer-Nielsen <andersfischern@me.com>
2015-10-08 23:36:06 +0200

    Implemented using ipc for receiving events from renderer process.

--- electron/main.js
+++ electron/main.js
@@ -7,25 +7,19 @@ var ipc = require('ipc');
 require('crash-reporter').start();
 
 var mainWindow = null;
-
-var req = request.post('http://localhost:8083/collection',
-    { body: '/collection.nml' },
-    function (error, response, body) {
-        if (!error && response.statusCode == 200) {
-            console.log(body)
-        }
-    }
-);
+var collection_path = ""
 
 app.on('ready', function() {
   	mainWindow = new BrowserWindow({width: 300, 'min-width': 300, height: 600});
   	mainWindow.loadUrl('file://' + __dirname + '/app/index.html');
 
   	mainWindow.on('closed', function() {
-		mainWindow = null;
+		  mainWindow = null;
   	});
 });
 
-ipc.on('collection-upload', function () {
-
+ipc.on('collection-upload', function (event, arg) {
+	collection_path = arg;
+    console.log(collection_path);
+    mainWindow.loadUrl('file://' + __dirname + '/app/index.html');
 });
