6b35884e566256a476e60c11446ea439eddc38fb
Anders Fischer-Nielsen <andersfischern@me.com>
2015-11-04 18:52:18 +0100

    Packaged first release!

---
+++ TraktorAutoNextSong.app/Contents/Resources/app/app/song-select.js
@@ -0,0 +1,98 @@
+'use strict';
+
+var ipc = require('ipc');
+
+var dropzone = document.getElementById('dropzone');
+
+dropzone.addEventListener('dragover', function(e) {
+    e.stopPropagation();
+    e.preventDefault();
+    e.dataTransfer.dropEffect = 'link';
+    dropzone.className = dropzone.className + ' dragover';
+});
+
+dropzone.addEventListener('dragleave', function(e) {
+    e.stopPropagation();
+    e.preventDefault();
+    dropzone.className = 'dropzone';
+});
+
+dropzone.addEventListener('drop', function(e) {
+    e.stopPropagation();
+    e.preventDefault();
+    dropzone.className = 'dropzone';
+    var file = e.dataTransfer.files[0];
+    var reader = new FileReader();
+    reader.onloadstart = function(e2) {
+        ipc.send('song-drop', file.name);
+    }
+    reader.readAsDataURL(file);
+});
+
+ipc.on('receive-transitions', function (arg) {
+    dropzone.style.height = '80px';
+    dropzone.style.boxShadow = 'box-shadow:inset 0px 0px 0px 2px lightgrey;'
+    document.getElementById('inner-dropzone').style.fontSize = '18px';
+    var received = JSON.parse(arg);
+    var song = received.song;
+    var transitions = received.transitions;
+    
+    setChosenSongInfo(song);
+    setTransitionInfo(transitions);
+});
+
+function setChosenSongInfo(song) {
+    var chosenTitle = document.getElementById('chosen-title');
+    chosenTitle.innerHTML = song.title;
+
+    var chosenArtist = document.getElementById('chosen-artist');
+    chosenArtist.innerHTML = song.artist;
+    
+    var chosenKey = document.getElementById('chosen-key');
+    chosenKey.innerHTML = song.key.item1 + song.key.item2.case[0];
+
+    var chosenBpm = document.getElementById('chosen-bpm');
+    chosenBpm.innerHTML = song.bpm;  
+}
+
+function setTransitionInfo(transitions) {
+    var list = document.getElementById('transition-list');
+
+    while (list.hasChildNodes()) {   
+        list.removeChild(list.firstChild);
+    }
+    
+    transitions.forEach(function(elem) {
+        var item = document.createElement('div');
+        item.className = 'list-item';
+
+        var title = document.createElement('div');
+        title.className = 'list-item-title ellipsis-overflow';
+
+        var artist = document.createElement('div');
+        artist.className = 'list-item-artist ellipsis-overflow';
+
+        var keyBpm = document.createElement('div');
+        keyBpm.className = 'list-item-key-bpm blue-color';
+
+        var bpm = document.createElement('div');
+        bpm.className = 'list-item-bpm';
+
+        var key = document.createElement('div');
+        key.className = 'list-item-key';
+
+        title.innerHTML = elem.title;
+        artist.innerHTML = elem.artist;
+        key.innerHTML = elem.key.item1 + elem.key.item2.case[0];
+        bpm.innerHTML = elem.bpm;
+
+        keyBpm.appendChild(key);
+        keyBpm.appendChild(bpm);
+
+        item.appendChild(title);
+        item.appendChild(artist);
+        item.appendChild(keyBpm);
+        
+        list.appendChild(item);
+    });
+}
---
+++ TraktorAutoNextSong.app/Contents/Resources/app/main.js
@@ -0,0 +1,85 @@
+var app = require('app'); 
+var BrowserWindow = require('browser-window'); 
+var request = require('request'); 
+var ipc = require('ipc');
+var crypto = require('crypto');
+var request = require('request');
+var dialog = require('dialog');
+var exec = require('child_process').exec;
+
+
+// Report crashes to our server.
+require('crash-reporter').start();
+
+var collection;
+
+var mainWindow = null;
+var collection_path = ""
+
+app.on('ready', function() {
+    mono = exec('mono ' + process.resourcesPath + '/Release/Traktor.exe', { cwd: undefined, env: '/usr/local/bin' }, function (error, stdout, stderr) {
+    	dialog.showErrorBox('err', error.message);
+    });
+
+  	mainWindow = new BrowserWindow({width: 350, height: 600, resizable: false});
+  	mainWindow.loadUrl('file://' + __dirname + '/app/index.html');
+
+  	mainWindow.on('closed', function() {
+		  mainWindow = null;
+  	});
+});
+
+app.on('quit', function() {
+    //process.kill(mono.pid, 1);
+	mono.kill('SIGINT');
+});
+
+app.on('window-all-closed', function() {
+	app.quit();
+});
+
+ipc.on('collection-upload', function (event, arg) {
+	collection_path = arg;
+	request.post({
+	  	headers: {'content-type' : 'application/x-www-form-urlencoded'},
+	  	url:     'http://localhost:8083/collection',
+	  	body:    arg
+	}, function(error, response, body) {
+		if (error != null) {
+			console.log(error);
+		}
+		
+		else {
+			if (response.statusCode != 200) {
+				console.log("Error: response was: " + response.statusCode);
+				mainWindow.loadUrl('file://' + __dirname + '/app/index.html');
+			}
+			
+			else {
+				event.sender.send('collection-uploaded');
+			}	
+		}
+	});
+});
+
+function createHash(s) {
+    var sha256 = crypto.createHash("sha256");
+    sha256.update(s, "utf8");
+    return sha256.digest("base64");
+}
+
+ipc.on('song-drop', function (event, arg) {
+	var hash = createHash(arg);
+	
+	request.get({
+	  	url:     'http://localhost:8083/choose/' + hash,
+	}, function(error, response, body) {
+		if (error != null) {
+			console.log(error);
+		}
+		
+		else {
+			event.sender.send('receive-transitions', body);
+		}
+	});
+});
