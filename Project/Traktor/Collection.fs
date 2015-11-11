b4cc37880007cd8587934ea32868e849f4b3e6ad
Anders Fischer-Nielsen <andersfischern@me.com>
2015-11-11 12:51:36 +0100

    Sorted bad results away when building graph in an attempt to fix memory issue. Made code more readable. Resolves #9

--- Project/Traktor/Collection.fs
+++ Project/Traktor/Collection.fs
@@ -9,28 +9,28 @@ open System
 type Collection = XmlProvider<"collection.nml">
 
 type Chord = Major | Minor | Invalid
-type Song = { BPM : decimal; Title : string; Artist : string; Key : (int * Chord); AudioId : string }
-type Edge = { Weight : double; From : Song; To : Song }
+type Song = { BPM : float; Title : string; Artist : string; Key : (int * Chord); AudioId : string }
+type Edge = { Weight : float; From : Song; To : Song }
 
-module CollectionParser = 
+module CollectionParser =
     open System.Text.RegularExpressions
     open System.Security.Cryptography
     open System.Text
 
     ///Hash a given string using SHA256 (UTF-8)
-    let hashString (s:string) = 
+    let hashString (s:string) =
         let sha256 = SHA256Managed.Create();
         let bytes = sha256.ComputeHash(Encoding.UTF8.GetBytes(s));
         Convert.ToBase64String(bytes);
 
     ///Unwrap a string option into either the value or "" if None.
-    let unwrapString s = 
+    let unwrapString s =
         match s with
         | Some e -> e
-        | None   -> ""
+        | None   -> " "
 
     ///Parse a .nml collection into a Song list.
-    let parseCollection (pathToCollection : string) = 
+    let parseCollection (pathToCollection : string) =
         let regex = Regex @"\d+"
 
         //Function wrapping parsing from one data type to another using TryParse.
@@ -40,7 +40,7 @@ module CollectionParser =
             | _ -> Failure
 
         ///Parse string keys from KEY.INFO attribute in NML.
-        let parseKey (s:string) = 
+        let parseKey (s:string) =
             let num = regex.Match s
             let key = if s.Contains("d") then Major else Minor
 
@@ -50,8 +50,8 @@ module CollectionParser =
             (number, key)
 
         ///Parse integer key from MUSICAL_KEY attribute in NML to Key.
-        let parseMusicalKey k = 
-            match k with 
+        let parseMusicalKey k =
+            match k with
             | 0     -> (1, Major) | 1     -> (8, Major)
             | 2     -> (3, Major) | 3     -> (10, Major)
             | 4     -> (5, Major) | 5     -> (12, Major)
@@ -67,28 +67,49 @@ module CollectionParser =
             | _     -> (0, Invalid)
 
         ///Parse a given NML Entry into a Song type.
-        let parseToSong (i:Collection.Entry) = 
+        let parseToSong (i:Collection.Entry) =
             match i.Tempo, i.Title.String, i.Artist, i.MusicalKey, i.Info.Key, i.Location.File with
             //Use the main MUSICAL_KEY attribute if possible.
             | Some te, Some ti, Some a, Some k, _, id
-                    -> { BPM = te.Bpm; Title = ti; Artist = a; Key = parseMusicalKey k.Value; AudioId = hashString id }
+                    -> { BPM = (float) te.Bpm; Title = ti; Artist = a;
+                         Key = parseMusicalKey k.Value;
+                         AudioId = hashString id }
             | Some te, _, Some a, Some k, _, id
-                    -> { BPM = te.Bpm; Title = unwrapString i.Title.String; Artist = a; Key = parseMusicalKey k.Value; AudioId = hashString id}
+                    -> { BPM = (float) te.Bpm; Title = unwrapString i.Title.String;
+                         Artist = a;
+                         Key = parseMusicalKey k.Value;
+                         AudioId = hashString id }
             | Some te, Some ti, _, Some k, _, id
-                    -> { BPM = te.Bpm; Title = ti; Artist = unwrapString i.Artist; Key = parseMusicalKey k.Value; AudioId = hashString id}
+                    -> { BPM = (float) te.Bpm; Title = ti; Artist = unwrapString i.Artist;
+                         Key = parseMusicalKey k.Value;
+                         AudioId = hashString id }
             | Some te,  _, _, Some k, _, id
-                    -> { BPM = te.Bpm; Title = unwrapString i.Title.String; Artist = unwrapString i.Artist; Key = parseMusicalKey k.Value; AudioId = hashString id}
+                    -> { BPM = (float) te.Bpm; Title = unwrapString i.Title.String;
+                         Artist = unwrapString i.Artist;
+                         Key = parseMusicalKey k.Value;
+                         AudioId = hashString id }
             //If MUSICAL_KEY isn't available use INFO.KEY
-            | Some te, Some ti, Some a, _, Some k, id 
-                    -> { BPM = te.Bpm; Title = ti; Artist = a; Key = parseKey k; AudioId = hashString id}
+            | Some te, Some ti, Some a, _, Some k, id
+                    -> { BPM = (float) te.Bpm; Title = ti; Artist = a;
+                         Key = parseKey k;
+                         AudioId = hashString id }
             | Some te, _, Some a, _, Some k, id
-                    -> { BPM = te.Bpm; Title = unwrapString i.Title.String; Artist = a; Key = parseKey k; AudioId = hashString id}
+                    -> { BPM = (float) te.Bpm; Title = unwrapString i.Title.String;
+                         Artist = a;
+                         Key = parseKey k;
+                         AudioId = hashString id }
             | Some te, Some ti, _, _, Some k, id
-                    -> { BPM = te.Bpm; Title = ti; Artist = unwrapString i.Artist; Key = parseKey k; AudioId = hashString id}
+                    -> { BPM = (float) te.Bpm; Title = ti; Artist = unwrapString i.Artist;
+                         Key = parseKey k;
+                         AudioId = hashString id }
             | Some te,  _, _, _, Some k, id
-                    -> { BPM = te.Bpm; Title = unwrapString i.Title.String; Artist = unwrapString i.Artist; Key = parseKey k; AudioId = hashString id}
+                    -> { BPM = (float) te.Bpm; Title = unwrapString i.Title.String;
+                         Artist = unwrapString i.Artist;
+                         Key = parseKey k;
+                         AudioId = hashString id }
             //Invalid key info.
-            | _, _, _, _, _, _  -> { BPM = new Decimal (0); Title = String.Empty; Artist = String.Empty; Key = (0, Invalid); AudioId = String.Empty}
+            | _     -> { BPM = 0.0; Title = String.Empty; Artist = String.Empty;
+                         Key = (0, Invalid); AudioId = String.Empty}
 
         let collection = Collection.Load(pathToCollection)
         let entries = collection.Collection.Entries2
@@ -96,17 +117,18 @@ module CollectionParser =
         List.ofArray songs
 
 
-module Graph = 
-    ///Create a graph (represented as a Song * Edge list array) from  a Song list. 
-    let buildGraph list = 
+module Graph =
+    ///Create a graph (represented as a Song * Edge list array) from  a Song list.
+    let buildGraph list =
         let original = list;
-        let withEdges = Array.Parallel.map (fun song -> let otherSongs = List.filter (fun s -> (s <> song)) original;
-                                                        let edgesFromSong = List.map (fun s -> { Weight = System.Double.MaxValue; From = song; To = s }) otherSongs
-                                                        (song, edgesFromSong)) <| Array.ofList list 
+        let withEdges = Array.Parallel.map (fun song ->
+          let otherSongs = List.filter (fun s -> (s <> song)) original;
+          let edgesFromSong = List.map (fun s -> { Weight = System.Double.MaxValue; From = song; To = s }) otherSongs
+          (song, edgesFromSong)) <| Array.ofList list
         withEdges
 
     ///Calculate the weight from a given Key to another Key.
-    let weightForKey key other = 
+    let weightForKey key other =
         let accountFor12 n = if n % 12 = 0 then 12 else n % 12
 
         let k = fst key
@@ -115,28 +137,30 @@ module Graph =
         let oneSemitone = accountFor12 k + 2        //One semitone up.
         let twoSemitones = accountFor12 k + 7       //two semitones up.
         let threeUpDown = match snd other with      //If minor, three keys UP, if major three keys DOWN.
-                          | Minor -> accountFor12 k + 3 
+                          | Minor -> accountFor12 k + 3
                           | Major -> accountFor12 k + 9
                           | Invalid  -> Int32.MaxValue
 
         let list = [ plusOne; minusOne; oneSemitone; twoSemitones; threeUpDown ] //Create a list of all good key transitions.
         let filtered = List.filter (fun x -> fst other = x) list                 //See if other key matches any of the good key transitions.
-        if filtered.IsEmpty then 20 else 0                                       //If there were any matches, then we're have a nice transition.
+        if filtered.IsEmpty then (float) 15 else 0.0                             //If there were any matches, then we're have a nice transition.
 
     ///Calculate weights for a (Song * Edge list) array
     let calculateWeights graph =
-        let calculateWeight edge = 
+        let calculateWeight edge =
             let bpmWeight = System.Math.Abs (edge.From.BPM - edge.To.BPM)
             let keyWeight = weightForKey edge.From.Key edge.To.Key
-            (double) bpmWeight + (double) keyWeight
+            (float) bpmWeight + keyWeight
 
-        let weightForSongEdgeTuple (song, edges) = 
-            (song, List.map (fun e -> { Weight = calculateWeight e; From = e.From; To = e.To }) edges)
-        
+        let weightForSongEdgeTuple (song, edges) =
+            let edges = List.map (fun e -> { Weight = calculateWeight e; From = e.From; To = e.To }) edges
+            let onlyBestResults = List.filter (fun x -> x.Weight < 30.0) edges
+            (song, onlyBestResults)
+
         let withWeights = Array.Parallel.map (fun x -> weightForSongEdgeTuple x) graph
         withWeights
 
     ///Create a Map<audioId:string, (Song * Edge list)> from a Song * Edge list array.
-    let asMap graph = 
+    let asMap graph =
         let mapped = Array.Parallel.map (fun x -> ((fst x).AudioId, x)) graph
         Map.ofArray mapped
