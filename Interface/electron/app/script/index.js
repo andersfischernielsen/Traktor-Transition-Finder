8336e9cf5c0b22473859fbc97543057dfc895ec1
Anders Fischer-Nielsen <andersfischern@me.com>
2016-08-24 17:28:14 +0200

    Compiled app using TypeScript.
    Woo, types!

--- Interface/electron/app/script/index.js
+++ Interface/electron/app/script/index.js
@@ -1,150 +1,134 @@
 'use strict';
-
-const electron = require('electron');
-const ipc = electron.ipcRenderer;
-const fs = require('fs');
-const remote = electron.remote;
-const dialog = remote.dialog;
-const app = remote.app;
-const Menu = remote.Menu;
-
-
+var electron = require('electron');
+var ipc = electron.ipcRenderer;
+var fs = require('fs');
+var remote = electron.remote;
+var dialog = remote.dialog;
+var app = remote.app;
+var Menu = remote.Menu;
 function setBodyDrag() {
-	var body = document.getElementsByTagName('body')[0];
-
-	//Make the main window ignore drag-n-drop.
-	body.addEventListener('dragover', function(e) {
-	    e.stopPropagation();
-	    e.preventDefault();
-	});
-
-	body.addEventListener('dragleave', function(e) {
-	    e.stopPropagation();
-	    e.preventDefault();
-	});
-
-	body.addEventListener('drop', function(e) {
-	    e.stopPropagation();
-	    e.preventDefault();
-	});
+    var body = document.getElementsByTagName('body')[0];
+    //Make the main window ignore drag-n-drop.
+    body.addEventListener('dragover', function (e) {
+        e.stopPropagation();
+        e.preventDefault();
+    });
+    body.addEventListener('dragleave', function (e) {
+        e.stopPropagation();
+        e.preventDefault();
+    });
+    body.addEventListener('drop', function (e) {
+        e.stopPropagation();
+        e.preventDefault();
+    });
 }
-
 //On button click in view, ask Electron main process to open file from file system.
 function openFile() {
- 	dialog.showOpenDialog(
-        remote.getCurrentWindow(),
-		{
-            filters: [ { name: 'Traktor Collection', extensions: ['nml']} ],
-            properties: [ 'openFile' ],
-            defaultPath: app.getPath('home') + '/Documents/Native Instruments/',
-        },
-        function (fileNames) {
-      		if (fileNames === undefined) return;
-      		var fileName = fileNames[0];
-      		ipc.send('collection-upload', fileName);
-  	    }
-)}
-
-function startSpinnerOnParsing() {
-	debugger;
-	var select = document.getElementById("collection-select");
-	select.parentNode.removeChild(select);
-	document.getElementById('spinner').className = 'spinner';
+    dialog.showOpenDialog(remote.getCurrentWindow(), {
+        filters: [{ name: 'Traktor Collection', extensions: ['nml'] }],
+        properties: ['openFile'],
+        defaultPath: app.getPath('home') + '/Documents/Native Instruments/',
+    }, function (fileNames) {
+        if (fileNames === undefined)
+            return;
+        var fileName = fileNames[0];
+        ipc.send('collection-upload', fileName);
+    });
 }
-
-ipc.on('parsing-started', function(event) {
-	startSpinnerOnParsing();
+function startSpinnerOnParsing() {
+    debugger;
+    var select = document.getElementById("collection-select");
+    select.parentNode.removeChild(select);
+    document.getElementById('spinner').className = 'spinner';
+}
+ipc.on('parsing-started', function (event) {
+    startSpinnerOnParsing();
 });
-
 //When collection has been uploaded, change view to drop state.
-ipc.on('collection-uploaded', function(event) {
+ipc.on('collection-uploaded', function (event) {
     var drop = document.getElementById("drop-song");
-	document.getElementById('spinner').className = '';
+    document.getElementById('spinner').className = '';
     drop.style.visibility = "visible";
 });
-
-
 function setMenu() {
-	var template = [
-		{
-			label: 'Traktor Auto Next Song',
-		    submenu: [
-		      	{
-	        		label: 'About Traktor Auto Next Song',
-	        		selector: 'orderFrontStandardAboutPanel:'
-		      	},
-				{
-	        		type: 'separator'
-		      	},
-		      	{
-	        		label: 'Preferences...',
-					accelerator: 'Command+,',
-					click: function() { ipc.send('preferences'); }
-		      	},
-		      	{
-				  	type: 'separator'
-		      	},
-		      	{
-	        		type: 'separator'
-		      	},
-		      	{
-	        		label: 'Services',
-	        		submenu: []
-		      	},
-		      	{
-				  	type: 'separator'
-		      	},
-		      	{
-		        	label: 'Hide Electron',
-		        	accelerator: 'Command+H',
-		        	selector: 'hide:'
-		      	},
-		      	{
-			        label: 'Hide Others',
-			        accelerator: 'Command+Shift+H',
-			        selector: 'hideOtherApplications:'
-		      	},
-		      	{
-			        label: 'Show All',
-			        selector: 'unhideAllApplications:'
-		      	},
-		      	{
-			        type: 'separator'
-		      	},
-		      	{
-		        	label: 'Quit',
-		        	accelerator: 'Command+Q',
-		        	selector: 'terminate:'
-		      	},
-		    ]
-		},
-		  {
-			  label: 'Window',
-			  submenu: [
-				  {
-					  label: 'Minimize',
-					  accelerator: 'Command+M',
-					  selector: 'performMiniaturize:'
-				  },
-				  {
-					  label: 'Close',
-					  accelerator: 'Command+W',
-					  selector: 'performClose:'
-				  },
-				  {
-					  type: 'separator'
-				  },
-				  {
-					  label: 'Bring All to Front',
-					  selector: 'arrangeInFront:'
-				  }
-			  ]
-		  }
-	  ];
-
+    var template = [
+        {
+            label: 'Traktor Auto Next Song',
+            submenu: [
+                {
+                    label: 'About Traktor Auto Next Song',
+                    selector: 'orderFrontStandardAboutPanel:'
+                },
+                {
+                    type: 'separator'
+                },
+                {
+                    label: 'Preferences...',
+                    accelerator: 'Command+,',
+                    click: function () { ipc.send('preferences'); }
+                },
+                {
+                    type: 'separator'
+                },
+                {
+                    type: 'separator'
+                },
+                {
+                    label: 'Services',
+                    submenu: []
+                },
+                {
+                    type: 'separator'
+                },
+                {
+                    label: 'Hide Electron',
+                    accelerator: 'Command+H',
+                    selector: 'hide:'
+                },
+                {
+                    label: 'Hide Others',
+                    accelerator: 'Command+Shift+H',
+                    selector: 'hideOtherApplications:'
+                },
+                {
+                    label: 'Show All',
+                    selector: 'unhideAllApplications:'
+                },
+                {
+                    type: 'separator'
+                },
+                {
+                    label: 'Quit',
+                    accelerator: 'Command+Q',
+                    selector: 'terminate:'
+                },
+            ]
+        },
+        {
+            label: 'Window',
+            submenu: [
+                {
+                    label: 'Minimize',
+                    accelerator: 'Command+M',
+                    selector: 'performMiniaturize:'
+                },
+                {
+                    label: 'Close',
+                    accelerator: 'Command+W',
+                    selector: 'performClose:'
+                },
+                {
+                    type: 'separator'
+                },
+                {
+                    label: 'Bring All to Front',
+                    selector: 'arrangeInFront:'
+                }
+            ]
+        }
+    ];
     var menu = Menu.buildFromTemplate(template);
-	Menu.setApplicationMenu(menu);
+    Menu.setApplicationMenu(menu);
 }
-
 setBodyDrag();
 setMenu();
--- Interface/electron/main.js
+++ Interface/electron/main.js
@@ -1,138 +1,117 @@
-const electron = require('electron');
-
-const app = electron.app;
-const BrowserWindow = electron.BrowserWindow;
-const ipc = electron.ipcMain;
-const dialog = electron.dialog;
-
-const crypto = require('crypto');
-const fs = require('fs');
-const request = require('request');
-const {exec} = require('child_process');
-const configuration = require('./configuration');
-
-let mainWindow;
-let preferencesWindow;
-let collectionPath;
-let graph;
-
-
-app.on('ready', function() {
-	if (process.platform === 'darwin') {
+"use strict";
+var electron = require('electron');
+var app = electron.app;
+var BrowserWindow = electron.BrowserWindow;
+var ipc = electron.ipcMain;
+var dialog = electron.dialog;
+var crypto = require('crypto');
+var fs = require('fs');
+var request = require('request');
+var exec = require('child_process').exec;
+var configuration = require('./configuration');
+var mainWindow;
+var preferencesWindow;
+var collectionPath;
+var graph;
+app.on('ready', function () {
+    if (process.platform === 'darwin') {
         graph = exec(process.resourcesPath + '/app/Release/Traktor', { cwd: undefined, env: '/usr/local/bin' }, function (error, stdout, stderr) {
-    		dialog.showErrorBox('Error', error.message);
-    	});
+            dialog.showErrorBox('Error', error.message);
+        });
     }
-
-  	mainWindow = new BrowserWindow({'min-width': 350, width: 400, height: 600, resizable: true});
-  	mainWindow.loadURL('file://' + __dirname + '/app/view/index.html');
-
-	function checkIfSettingsPresent() {
-		var collectionPath = configuration.readSettings('collectionPath');
-		if (collectionPath) {
-			mainWindow.webContents.on('did-finish-load', function() {
-				sendCollectionRequest(collectionPath);
-			})
-		}
-	}
-
-	checkIfSettingsPresent();
-
+    mainWindow = new BrowserWindow({ minWidth: 350, width: 400, height: 600, resizable: true });
+    mainWindow.loadURL('file://' + __dirname + '/app/view/index.html');
+    function checkIfSettingsPresent() {
+        var collectionPath = configuration.readSettings('collectionPath');
+        if (collectionPath) {
+            mainWindow.webContents.on('did-finish-load', function () {
+                sendCollectionRequest(collectionPath);
+            });
+        }
+    }
+    checkIfSettingsPresent();
     mainWindow.webContents.openDevTools();
-
-  	mainWindow.on('closed', function() {
-		if (preferencesWindow != null) {
-			preferencesWindow.close();
-		}
-		preferencesWindow = null;
-		mainWindow = null;
-  	});
+    mainWindow.on('closed', function () {
+        if (preferencesWindow != null) {
+            preferencesWindow.close();
+        }
+        preferencesWindow = null;
+        mainWindow = null;
+    });
 });
-
-app.on('quit', function() {
-	graph.kill('SIGKILL');
+app.on('quit', function () {
+    graph.kill('SIGKILL');
 });
-
-app.on('window-all-closed', function() {
-	app.quit();
+app.on('window-all-closed', function () {
+    app.quit();
 });
-
 ipc.on('collection-upload', function (event, path) {
-	sendCollectionRequest(path);
-	collectionPath = path;
+    sendCollectionRequest(path);
+    collectionPath = path;
 });
-
 function sendCollectionRequest(path) {
-	var edges = configuration.readSettings('numberOfEdges');
-	var responseBody = null;
-	if (typeof edges != 'undefined') {
-		responseBody = { collectionPath: path, numberOfEdges: edges};
-	}
-	else {
-		responseBody = { collectionPath : path };
-	}
-
-	mainWindow.webContents.send('parsing-started');
-
-	request.post({
-	  	headers: {'content-type' : 'application/x-www-form-urlencoded'},
-	  	url:     'http://localhost:8083/collection',
-	  	body:    JSON.stringify(responseBody)
-	}, function(error, response, body) {
-		if (error != null) {
-    		dialog.showErrorBox('F# Server Error', error.message);
-		}
-
-		else {
-			if (response.statusCode != 200) {
-				console.log("Error: response was: " + response.statusCode);
-				mainWindow.loadUrl('file://' + __dirname + '/app/view/index.html');
-			}
-
-			else {
-				mainWindow.webContents.send('collection-uploaded');
-			}
-		}
-	});
+    var edges = configuration.readSettings('numberOfEdges');
+    var responseBody = null;
+    if (typeof edges != 'undefined') {
+        responseBody = { collectionPath: path, numberOfEdges: edges };
+    }
+    else {
+        responseBody = { collectionPath: path };
+    }
+    mainWindow.webContents.send('parsing-started');
+    request.post({
+        headers: { 'content-type': 'application/x-www-form-urlencoded' },
+        url: 'http://localhost:8083/collection',
+        body: JSON.stringify(responseBody)
+    }, function (error, response, body) {
+        if (error != null) {
+            dialog.showErrorBox('F# Server Error', error.message);
+        }
+        else {
+            if (response.statusCode != 200) {
+                console.log("Error: response was: " + response.statusCode);
+                mainWindow.loadUrl('file://' + __dirname + '/app/view/index.html');
+            }
+            else {
+                mainWindow.webContents.send('collection-uploaded');
+            }
+        }
+    });
 }
-
 function createHash(s) {
     var sha256 = crypto.createHash("sha256");
     sha256.update(s, "utf8");
     return sha256.digest("base64");
 }
-
 ipc.on('song-drop', function (event, fileName, hash) {
-	if (fileName) hash = createHash(fileName);
-	var transitions = configuration.readSettings('transitions');
-	if (typeof transitions === 'undefined') transitions = 8;
-
-	var url = 'http://localhost:8083/choose/' + transitions + '/' + hash;
-	request.get({
-	  	url:     url,
-	}, function(error, response, body) {
-		if (error != null) {
-			dialog.showErrorBox('F# Server Error', error.message);
-		}
-
-		else {
-			event.sender.send('receive-transitions', body);
-		}
-	});
+    if (fileName)
+        hash = createHash(fileName);
+    var transitions = configuration.readSettings('transitions');
+    if (typeof transitions === 'undefined')
+        transitions = 8;
+    var url = 'http://localhost:8083/choose/' + transitions + '/' + hash;
+    request.get({
+        url: url,
+    }, function (error, response, body) {
+        if (error != null) {
+            dialog.showErrorBox('F# Server Error', error.message);
+        }
+        else {
+            event.sender.send('receive-transitions', body);
+        }
+    });
 });
-
 ipc.on('preferences', function (event, arg) {
-	if (preferencesWindow) return;
-
-	preferencesWindow = new BrowserWindow({width: 530, height: 270, resizable: false});
-  	preferencesWindow.loadURL('file://' + __dirname + '/app/view/preferences.html');
+    if (preferencesWindow)
+        return;
+    preferencesWindow = new BrowserWindow({ width: 530, height: 270, resizable: false });
+    preferencesWindow.loadURL('file://' + __dirname + '/app/view/preferences.html');
     preferencesWindow.webContents.openDevTools();
-
-	preferencesWindow.on('closed', function() {
-		preferencesWindow = null;
-	})
+    preferencesWindow.on('closed', function () {
+        preferencesWindow = null;
+    });
 });
-
 ipc.on('collection-path-request', function (event) {
-	if (collectionPath) event.sender.send('receive-collection-path', collectionPath);
+    if (collectionPath)
+        event.sender.send('receive-collection-path', collectionPath);
 });
