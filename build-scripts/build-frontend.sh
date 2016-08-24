71de90ffff7705a3e88dbc1754bdfeee08d88711
Anders Fischer-Nielsen <andersfischern@me.com>
2016-08-24 18:10:15 +0200

    Fixed typing errors.

--- build-scripts/build-frontend.sh
+++ build-scripts/build-frontend.sh
@@ -1,11 +1,16 @@
 cd ../Interface/electron
 npm install
 typings install debug --save
+tsc
 
 cd ../../build-scripts
 rm -r TraktorTransitionFinder.app
 cp -r Electron.app TraktorTransitionFinder.app
-cp -a ../Interface/electron/. TraktorTransitionFinder.app/Contents/Resources/app/
-cp -a ../Graph/Traktor/bin/Release TraktorTransitionFinder.app/Contents/Resources/app/
+
+cp -a ../Interface/electron TraktorTransitionFinder.app/Contents/Resources/app
+
+mkdir TraktorTransitionFinder.app/Contents/Resources/app/Release/
+cp -a ../Graph/Traktor/bin/Release/Traktor TraktorTransitionFinder.app/Contents/Resources/app/Release/Traktor
+
 cp ../Interface/electron/Info.plist TraktorTransitionFinder.app/Contents/Info.plist
 cp ../Interface/electron/atom.icns TraktorTransitionFinder.app/Contents/Resources/atom.icns
