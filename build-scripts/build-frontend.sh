12b2a8be8a26e73a188c37810913a4aa6f9c96ed
Anders Fischer-Nielsen <andersfischern@me.com>
2016-08-24 16:45:23 +0200

    Fixed frontend script.

--- build-scripts/build-frontend.sh
+++ build-scripts/build-frontend.sh
@@ -1,8 +1,8 @@
 cd ../Interface/electron
 npm install
-cd ../../build
-cp -r Electron.app TraktorAutoNextSong.app
-cp -a ../Interface/electron/. TraktorAutoNextSong.app/Contents/Resources/app/
-cp -a ../Graph/Traktor/bin/Release TraktorAutoNextSong.app/Contents/Resources/app/
-cp ../Interface/electron/Info.plist TraktorAutoNextSong.app/Contents/Info.plist
-cp ../Interface/electron/atom.icns TraktorAutoNextSong.app/Contents/Resources/atom.icns
+cd ../../build-scripts
+cp -r Electron.app TraktorTransitionFinder.app
+cp -a ../Interface/electron/. TraktorTransitionFinder.app/Contents/Resources/app/
+cp -a ../Graph/Traktor/bin/Release TraktorTransitionFinder.app/Contents/Resources/app/
+cp ../Interface/electron/Info.plist TraktorTransitionFinder.app/Contents/Info.plist
+cp ../Interface/electron/atom.icns TraktorTransitionFinder.app/Contents/Resources/atom.icns
