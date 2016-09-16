8563cb2df3f32c8b6155fe6c1b1aca4ed40de949
Anders Fischer-Nielsen <andersfischern@me.com>
2016-09-16 14:47:11 +0200

    Rewrote build script.

--- build-scripts/build-backend.sh
+++
@@ -1,8 +0,0 @@
-cd ../Graph/
-nuget restore
-xbuild /p:Configuration=Release ../Graph/TraktorAutoNextSong.sln
-export PATH=/Library/Frameworks/Mono.framework/Commands:$PATH
-export AS="as -arch i386"
-export CC="cc -arch i386 -framework CoreFoundation -lobjc -liconv"
-cd Traktor/bin/Release/
-mkbundle -o Traktor Traktor.exe Suave.dll FsPickler.dll FSharp.Data.dll FSharp.Core.dll Newtonsoft.Json.dll --deps
--- build-scripts/build.sh
+++ build-scripts/build.sh
@@ -9,8 +9,5 @@ cp -r Electron.app TraktorTransitionFinder.app
 
 cp -a ../Interface/electron TraktorTransitionFinder.app/Contents/Resources/app
 
-mkdir TraktorTransitionFinder.app/Contents/Resources/app/Release/
-cp -a ../Graph/Traktor/bin/Release/Traktor TraktorTransitionFinder.app/Contents/Resources/app/Release/Traktor
-
 cp ../Interface/electron/Info.plist TraktorTransitionFinder.app/Contents/Info.plist
 cp ../Interface/electron/atom.icns TraktorTransitionFinder.app/Contents/Resources/atom.icns
