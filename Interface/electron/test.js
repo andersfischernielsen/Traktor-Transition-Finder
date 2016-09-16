42a786bcfe7b4aee9e70233a5cf371382f97cc92
Anders Fischer-Nielsen <andersfischern@me.com>
2016-09-16 14:53:55 +0200

    Removed .js files.

--- Interface/electron/test.js
+++
@@ -1,5 +0,0 @@
-var graph = require('./graph.js');
-var parsed = graph.parseCollection('/Users/Anders/Documents/Native Instruments/Traktor 2.10.2/collection.nml');
-var result = graph.buildGraph(parsed, 5);
-var asMap = graph.asMap(result);
-console.log("test");
