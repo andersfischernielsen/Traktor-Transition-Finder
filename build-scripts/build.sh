2e97d195345ef535b1ec7b0368a48d99275b41f9
Anders Fischer-Nielsen <andersfischern@me.com>
2016-09-16 14:57:56 +0200

    Reordered project structure.

--- build-scripts/build.sh
+++ build-scripts/build.sh
@@ -1,13 +1,13 @@
-cd ../Interface/electron
+cd ../electron
 npm install
 typings install debug --save
 tsc
 
-cd ../../build-scripts
+cd ../build-scripts
 rm -r TraktorTransitionFinder.app
 cp -r Electron.app TraktorTransitionFinder.app
 
-cp -a ../Interface/electron TraktorTransitionFinder.app/Contents/Resources/app
+cp -a ../electron TraktorTransitionFinder.app/Contents/Resources/app
 
-cp ../Interface/electron/Info.plist TraktorTransitionFinder.app/Contents/Info.plist
-cp ../Interface/electron/atom.icns TraktorTransitionFinder.app/Contents/Resources/atom.icns
+cp ../electron/Info.plist TraktorTransitionFinder.app/Contents/Info.plist
+cp ../electron/atom.icns TraktorTransitionFinder.app/Contents/Resources/atom.icns
