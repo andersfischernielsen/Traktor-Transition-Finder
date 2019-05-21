41a5582f77f65df3097c5550f88c9a5ee4836420
Anders Fischer <andersfischern@me.com>
2019-05-22 00:41:49 +1200

    Fixed a few bugs in the graph generation.

--- swift/Traktor_Transition_Finder/Traktor Transition Finder/DragDropViewController.swift
+++ swift/Traktor_Transition_Finder/Traktor Transition Finder/DragDropViewController.swift
@@ -1,40 +1,94 @@
+import Foundation
 import Cocoa
 
 
 class DragDropViewController: NSViewController {
     @IBOutlet var dropZone: DestinationView!
     @IBOutlet weak var transitionsTableView: NSTableView!
-    
+    @IBOutlet weak var dropTextField: NSTextField!
     var transitions = [String]()
-    
-    
-  enum Appearance {
-    static let shadowOpacity: Float =  0.4
-    static let shadowOffset: CGFloat = 4
-  }
-
-  override func viewDidLoad() {
-    super.viewDidLoad()
-    dropZone.delegate = self
-  }
-  
-  
-  
-  func configureShadow(_ view: NSView) {
-    if let layer = view.layer {
-      layer.shadowColor = NSColor.black.cgColor
-      layer.shadowOpacity = Appearance.shadowOpacity
-      layer.shadowOffset = CGSize(width: Appearance.shadowOffset, height: -Appearance.shadowOffset)
-      layer.masksToBounds = false
+    var graph: [String: (Song, [Edge])] = [:]
+    var collectionURL: URL? {
+        didSet {
+            if collectionURL == nil {
+                self.view.window?.level = NSWindow.Level.init(rawValue: 0)
+            }
+            else {
+                updateCollection(path: collectionURL)
+            }
+        }
     }
-  }
+    
+    enum Appearance {
+        static let shadowOpacity: Float =  0.4
+        static let shadowOffset: CGFloat = 4
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        dropZone.delegate = self
+        if (collectionURL == nil) {
+            selectCollection()
+        }
+    }
+    
+    func updateCollection(path: URL?) {
+        DispatchQueue.global(qos: .background).async {
+            DispatchQueue.main.async {
+                self.dropTextField.stringValue = "Parsing Traktor Collection..."
+            }
+            let parsed = CollectionParser.parseCollection(pathToCollection: self.collectionURL!)
+            DispatchQueue.main.async {
+                self.dropTextField.stringValue = "Building Song Graph..."
+            }
+            let graph = Graph.buildGraph(list: parsed, numberOfEdges: 5)
+            self.graph = graph
+            
+            DispatchQueue.main.async {
+                self.dropTextField.stringValue = "Drop Songs Here"
+            }
+        }
+        
+        
+    }
+
+    func selectCollection() {
+        let openPanel = NSOpenPanel()
+        openPanel.allowsMultipleSelection = false
+        openPanel.canChooseDirectories = false
+        openPanel.canCreateDirectories = false
+        openPanel.canChooseFiles = true
+        openPanel.allowedFileTypes = ["nml"]
+        openPanel.level = NSWindow.Level.init(rawValue: 1)
+        openPanel.begin { (result) -> Void in
+            if result == .OK {
+                self.collectionURL = openPanel.url
+            }
+            else if self.collectionURL == nil {
+                NSApplication.shared.terminate(self)
+            }
+        }
+    }
+  
+  
+    func configureShadow(_ view: NSView) {
+        if let layer = view.layer {
+          layer.shadowColor = NSColor.black.cgColor
+          layer.shadowOpacity = Appearance.shadowOpacity
+          layer.shadowOffset = CGSize(width: Appearance.shadowOffset, height: -Appearance.shadowOffset)
+          layer.masksToBounds = false
+        }
+    }
 }
 
 extension DragDropViewController: DestinationViewDelegate {
   
   func processFileURLs(_ urls: [URL]) {
-    transitions.append(contentsOf: urls.map({$0.absoluteString}))
-    transitionsTableView.dataSource = self
+    let key = urls.first!
+        .absoluteString.replacingOccurrences(of: "file://", with: "")
+        .replacingOccurrences(of: " ", with: "%20")
+    transitions = (graph[key]?.1.map { e in return e.To.AudioId })!
+    transitionsTableView.dataSource = self as NSTableViewDataSource
   }
 }
 
@@ -47,7 +101,6 @@ extension DragDropViewController: NSTableViewDataSource {
 extension DragDropViewController: NSTableViewDelegate {
   func tableView(_ tableView: NSTableView, viewFor tableColumn: NSTableColumn?, row: Int) -> NSView? {
     let item = transitions[row]
-    print(item)
     let cell = tableView.makeView(withIdentifier: (tableColumn!.identifier), owner: self) as? NSTableCellView
     cell?.textField?.stringValue = item
     return cell
--- swift/Traktor_Transition_Finder/Traktor Transition Finder/Graph.swift
+++ swift/Traktor_Transition_Finder/Traktor Transition Finder/Graph.swift
@@ -4,8 +4,7 @@
 //
 
 import Foundation
-import Foundation.NSRegularExpression
-import Ji
+import SWXMLHash
 
 enum Chord { case Major,  Minor, Invalid }
 struct Song {
@@ -35,203 +34,177 @@ struct XMLEntry {
     let AudioId: String?
 }
 
-//The "punishment" for having a bad key transition.
-let BADKEYWEIGHT = 15.0
-
 class CollectionParser {
+    ///Parse a .nml collection into a Song list.
+    static func parseCollection(pathToCollection: URL) -> [Song] {
         func parseXML(path: URL) -> [XMLEntry] {
-            var entries: [XMLEntry] = []
             do {
                 let file = try String(contentsOf: path, encoding: .utf8)
-                let document = Ji(xmlString: file)
-                if let documentEntries = document?.xPath("//ENTRY") {
+                let replaced = file.replacingOccurrences(of: "\n", with: "")
+                let document = SWXMLHash.lazy(replaced)
+                return document["NML"]["COLLECTION"]["ENTRY"].all.map { (entry) -> XMLEntry in
                     //TODO: Convert into map()
-                    for entry in documentEntries {
-                        let te = entry.xPath("//TEMPO/@BPM")[0].content
-                        let ti = entry.xPath("//ENTRY/@TITLE")[0].content
-                        let a = entry.xPath("//ENTRY/@ARTIST")[0].content
-                        let mk = entry.xPath("//MUSICAL_KEY/@VALUE")[0].content
-                        let ik = entry.xPath("//INFO/@KEY")[0].content
-                        let id = entry.xPath("//LOCATION/@FILE")[0].content
-                        entries.append(XMLEntry(BPM: te, Title: ti, Artist: a, MusicalKey: mk, InfoKey: ik, AudioId: id))
+                    let ti: String? = entry.value(ofAttribute: "TITLE")
+                    let ar: String? = entry.value(ofAttribute: "ARTIST")
+                    var id: String?
+                    var te: String?
+                    var mk: String?
+                    var ik: String?
+                    for child in entry.children {
+                        te = child.value(ofAttribute: "BPM") ?? te
+                        mk = child.value(ofAttribute:"VALUE") ?? mk
+                        ik = child.value(ofAttribute: "KEY") ?? ik
+                        if let d: String = child.value(ofAttribute: "DIR"),
+                           let f: String = child.value(ofAttribute: "FILE") {
+                            id = (d + f).replacingOccurrences(of: ":", with: "")
+                        }
                     }
+                    return XMLEntry(BPM: te, Title: ti, Artist: ar, MusicalKey: mk, InfoKey: ik, AudioId: id)
                 }
             } catch let error {
                 print(error)
             }
-            return entries
+            return []
         }
-
-    ///Parse string keys from KEY.INFO attribute in NML.
-    func parseKey(s: String) -> (Int, Chord) {
-        func matches(for regex: String, in text: String) -> [String] {
-            do {
-                let regex = try NSRegularExpression(pattern: regex)
-                let results = regex.matches(in: text,
-                        range: NSRange(text.startIndex..., in: text))
-                return results.map {
-                    String(text[Range($0.range, in: text)!])
+        
+        ///Parse string keys from KEY.INFO attribute in NML.
+        func parseKey(s: String) -> (Int, Chord) {
+            func matches(for regex: String, in text: String) -> [String] {
+                do {
+                    let regex = try NSRegularExpression(pattern: regex)
+                    let results = regex.matches(in: text,
+                                                range: NSRange(text.startIndex..., in: text))
+                    return results.map {
+                        String(text[Range($0.range, in: text)!])
+                    }
+                } catch let error {
+                    print("invalid regex: \(error.localizedDescription)")
+                    return []
                 }
-            } catch let error {
-                print("invalid regex: \(error.localizedDescription)")
-                return []
+            }
+            
+            let regex = matches(for: "+d", in: s)
+            let num = Int(regex[0]) ?? 0
+            let key: Chord = s.contains("d") ? Chord.Major : Chord.Minor
+            return (num, key)
+        }
+        
+        ///Parse integer key from MUSICAL_KEY attribute in NML to Key.
+        func parseMusicalKey(k: Int) -> (Int, Chord) {
+            switch k {
+                case 0: return (1, Chord.Major)
+                case 1: return (8, Chord.Major)
+                case 2: return (3, Chord.Major)
+                case 3: return (10, Chord.Major)
+                case 4: return (5, Chord.Major)
+                case 5: return (12, Chord.Major)
+                case 6: return (7, Chord.Major)
+                case 7: return (2, Chord.Major)
+                case 8: return (9, Chord.Major)
+                case 9: return (4, Chord.Major)
+                case 10: return (11, Chord.Major)
+                case 11: return (6, Chord.Major)
+                case 12: return (10, Chord.Minor)
+                case 13: return (5, Chord.Minor)
+                case 14: return (12, Chord.Minor)
+                case 15: return (7, Chord.Minor)
+                case 16: return (2, Chord.Minor)
+                case 17: return (9, Chord.Minor)
+                case 18: return (4, Chord.Minor)
+                case 19: return (11, Chord.Minor)
+                case 20: return (6, Chord.Minor)
+                case 21: return (1, Chord.Minor)
+                case 22: return (8, Chord.Minor)
+                case 23: return (3, Chord.Minor)
+                default: return (0, Chord.Invalid)
             }
         }
-
-        let regex = matches(for: "+d", in: s)
-        let num = Int(regex[0]) ?? 0
-        let key: Chord = s.contains("d") ? Chord.Major : Chord.Minor
-        return (num, key)
-    }
-
-    ///Parse integer key from MUSICAL_KEY attribute in NML to Key.
-    func parseMusicalKey(k: Int) -> (Int, Chord) {
-        switch k {
-            case 0:
-                return (1, Chord.Major)
-            case 1:
-                return (8, Chord.Major)
-            case 2:
-                return (3, Chord.Major)
-            case 3:
-                return (10, Chord.Major)
-            case 4:
-                return (5, Chord.Major)
-            case 5:
-                return (12, Chord.Major)
-            case 6:
-                return (7, Chord.Major)
-            case 7:
-                return (2, Chord.Major)
-            case 8:
-                return (9, Chord.Major)
-            case 9:
-                return (4, Chord.Major)
-            case 10:
-                return (11, Chord.Major)
-            case 11:
-                return (6, Chord.Major)
-            case 12:
-                return (10, Chord.Minor)
-            case 13:
-                return (5, Chord.Minor)
-            case 14:
-                return (12, Chord.Minor)
-            case 15:
-                return (7, Chord.Minor)
-            case 16:
-                return (2, Chord.Minor)
-            case 17:
-                return (9, Chord.Minor)
-            case 18:
-                return (4, Chord.Minor)
-            case 19:
-                return (11, Chord.Minor)
-            case 20:
-                return (6, Chord.Minor)
-            case 21:
-                return (1, Chord.Minor)
-            case 22:
-                return (8, Chord.Minor)
-            case 23:
-                return (3, Chord.Minor)
-            default:
-                return (0, Chord.Invalid)
-    }
-
-    ///Parse a given NML Entry into a Song type.
-    func parseToSong(entry: XMLEntry) -> Song {
-        if (entry.BPM == nil || entry.AudioId == nil) {
-            return Song(BPM: 0, Title: "", Artist: "", Key: (0, Chord.Invalid), AudioId: "")
+        
+        ///Parse a given NML Entry into a Song type.
+        func parseToSong(entry: XMLEntry) -> Song {
+            if (entry.BPM == nil || entry.AudioId == nil) {
+                return Song(BPM: 0, Title: "", Artist: "", Key: (0, Chord.Invalid), AudioId: "")
+            }
+            
+            let te = Double(entry.BPM!)!
+            let id = entry.AudioId!
+            let ti = entry.Title ?? ""
+            let ar = entry.Artist ?? ""
+            
+            if entry.MusicalKey != nil {
+                let key = Int(entry.MusicalKey!)!
+                return Song(BPM: te, Title: ti, Artist: ar, Key: parseMusicalKey(k: key), AudioId: id)
+            } else {
+                let key = entry.InfoKey!
+                return Song(BPM: te, Title: ti, Artist: ar, Key: parseKey(s: key), AudioId: id)
+            }
         }
         
-        let te = Double(entry.BPM!)!
-        let ti = entry.Title!
-        let ar = entry.Artist!
-        let id = entry.AudioId!
-        
-        if entry.MusicalKey != nil {
-            let key = Int(entry.MusicalKey!)!
-            return Song(BPM: te, Title: ti, Artist: ar, Key: parseMusicalKey(k: key), AudioId: id)
-        } else {
-            let key = entry.InfoKey!
-            return Song(BPM: te, Title: ti, Artist: ar, Key: parseKey(s: key), AudioId: id)
-        }
-    }
-
-        ///Parse a .nml collection into a Song list.
-    func parseCollection(pathToCollection: String) -> [Song] {
         var songs: [Song] = []
-        let url = URL(fileURLWithPath: pathToCollection)
-        let xmlEntries = parseXML(path: url)
+        let xmlEntries = parseXML(path: pathToCollection)
         for entry in xmlEntries {
             songs.append(parseToSong(entry: entry))
         }
         return songs
     }
 }
-}
 
 class Graph {
-    ///Calculate the weight from a given Key to another Key.
-    func weightForKey(key: (Int, Chord), other: (Int, Chord)) -> Double {
-        func accountFor12 (n: Int) -> Int {
-                return (n % 12 == 0 ? 12 : n % 12)
-            }
-        let plusOne = accountFor12(n: key.0 + 1) //One key up
-        let minusOne = accountFor12(n: key.0 + 11) //One key down.
-        let oneSemitone = accountFor12(n: key.0 + 2) //One semitone up.
-        let twoSemitones = accountFor12(n: key.0 + 7) //two semitones up.
-        func threeUpDown (k: Chord) -> Int {
-            //If Chord.Minor, three keys UP, if Chord.Major three keys DOWN.
-            if k == Chord.Minor {
-                return accountFor12(n: key.0 + 3)
-            }
-            else if k == Chord.Major {
-                return accountFor12(n: key.0 + 9)
-            }
-            else {
-                return Int.max
-            }
-        }
-        
-        //Create a list of all good key transitions.
-        let lst = [
-            plusOne,
-            minusOne,
-            oneSemitone,
-            twoSemitones,
-            threeUpDown(k: other.1)
-        ]
-        
-        //See if other key matches any of the good key transitions.
-        let filtered = lst.filter({(x: Int) -> Bool in other.0 == x})
-        //If there were any matches, then it's a nice key transition.
-        return filtered.count == 0 ? BADKEYWEIGHT : 0.0
-    }
-
-    func generateEdge(fromSong: Song, toSong: Song) -> Edge {
-        let bpmDifference = abs(fromSong.BPM - toSong.BPM)
-        let keyWeight = weightForKey(key: fromSong.Key, other: toSong.Key)
-
-        let weight = bpmDifference + keyWeight
-        return Edge (Weight: weight, From: fromSong, To: toSong)
-    }
-
-    ///Create a Map<audioId:string, (Song * Edge list)> from a Song * Edge list array.
-    func asMap(graph: [(Song, [Edge])]) -> [String: (Song, [Edge])] {
-        var m: [String: (Song, [Edge])] = [:]
-        graph.forEach({ m[$0.0.AudioId] = $0 })
-        return m
-    }
-    
     ///Calculate weights for a (Song * Edge list) array.
     ///Create a graph (represented as a Song * Edge list array) from  a Song list.
-    func buildGraph(list: [Song], numberOfEdges: Int) -> (Song, [Edge]) {
+    static func buildGraph(list: [Song], numberOfEdges: Int) -> [String: (Song, [Edge])] {
+        //The "punishment" for having a bad key transition.
+        let BADKEYWEIGHT: Double = 15.0
+        
         func generateEdgesForSong(song: Song, songs: [Song]) -> (Song, [Edge]) {
             func createEdgesFromSong(songs: [Song]) -> (Song, [Edge]) {
-                let otherSongs = songs.filter({ $0 != song })
-                let withEdges = otherSongs.map( { generateEdge(fromSong: song, toSong: $0) })
+                func generateEdge(fromSong: Song, toSong: Song) -> Edge {
+                    ///Calculate the weight from a given Key to another Key.
+                    func weightForKey(key: (Int, Chord), other: (Int, Chord)) -> Double {
+                        func accountFor12 (n: Int) -> Int {
+                            return (n % 12 == 0 ? 12 : n % 12)
+                        }
+                        let plusOne = accountFor12(n: key.0 + 1) //One key up
+                        let minusOne = accountFor12(n: key.0 + 11) //One key down.
+                        let oneSemitone = accountFor12(n: key.0 + 2) //One semitone up.
+                        let twoSemitones = accountFor12(n: key.0 + 7) //two semitones up.
+                        func threeUpDown (k: Chord) -> Int {
+                            //If Chord.Minor, three keys UP, if Chord.Major three keys DOWN.
+                            if k == Chord.Minor {
+                                return accountFor12(n: key.0 + 3)
+                            }
+                            else if k == Chord.Major {
+                                return accountFor12(n: key.0 + 9)
+                            }
+                            else {
+                                return Int.max
+                            }
+                        }
+                        
+                        //Create a list of all good key transitions.
+                        let lst = [
+                            plusOne,
+                            minusOne,
+                            oneSemitone,
+                            twoSemitones,
+                            threeUpDown(k: other.1)
+                        ]
+                        
+                        //See if other key matches any of the good key transitions.
+                        let filtered = lst.filter({(x: Int) -> Bool in other.0 == x})
+                        //If there were any matches, then it's a nice key transition.
+                        return filtered.count == 0 ? BADKEYWEIGHT : 0.0
+                    }
+                    
+                    let bpmDifference = abs(fromSong.BPM - toSong.BPM)
+                    let keyWeight = weightForKey(key: fromSong.Key, other: toSong.Key)
+                    
+                    let weight = bpmDifference + keyWeight
+                    return Edge (Weight: weight, From: fromSong, To: toSong)
+                }
+                
+                //let otherSongs = songs.filter({ (element) -> Bool in element != song })
+                let withEdges = songs.map( { generateEdge(fromSong: song, toSong: $0) })
                 let sorted = withEdges.sorted(by: { $0.Weight < $1.Weight })
                 return (song, Array(sorted.prefix(numberOfEdges)))
             }
@@ -239,7 +212,15 @@ class Graph {
             return createEdgesFromSong(songs: songs)
         }
         
-        let result = list.map({ generateEdgesForSong(song: $0, songs: list) })[0]
-        return result
+        ///Create a Map<audioId:string, (Song * Edge list)> from a Song * Edge list array.
+        func asMap(graph: [(Song, [Edge])]) -> [String: (Song, [Edge])] {
+            var m: [String: (Song, [Edge])] = [:]
+            graph.forEach({ m[$0.0.AudioId] = $0 })
+            return m
+        }
+        
+        let result = list.map({ generateEdgesForSong(song: $0, songs: list) })
+        let graph = asMap(graph: result)
+        return graph
     }
 }
